215ba359280382f0eef5974f2a2e260ce2fe5b69
BT22104001 <119791271+Varun03697@users.noreply.github.com>
2022-12-22 20:42:52 +0530

    Delete question no 4.py

--- question no 4.py
+++
@@ -1,5 +0,0 @@
-#4
-FirstNumber=int(input("Enter the number 1:"))
-SecondNumber=int(input("Enter the number 2:"))
-ThirdNumber=int(input("Enter the number 3:"))
-print(max(FirstNumber,SecondNumber,ThirdNumber))
