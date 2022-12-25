8313d1cf67723f5e0ca367963beb75e2d4d0b994
BT22104001 <119791271+Varun03697@users.noreply.github.com>
2022-12-25 19:58:32 +0530

    Add files via upload

---
+++ Assignmemt 3 Q3.py
@@ -0,0 +1,6 @@
+list=[]
+for i in range(3):
+    x = int(input("Enter the number:"))
+    list.append((x,x**2))
+print(list)
+
---
+++ Assignment 3 Q1.py
@@ -0,0 +1,6 @@
+#1
+sentence=input("Enter the string:")
+
+word=input("Enter the word:")
+
+print(sentence.count(word))
